9918604982984c837a3161969628f99e0295feec
Yongkang Zhao <cookieyong@gmail.com>
2020-11-19 15:52:47 -0800

    added another useEffect to handle delay and avoid double query when results.length change when the page first loads and first gets populated

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,9 +1,21 @@
 import React, {useState, useEffect} from 'react'
 import axios from 'axios';
-
 const Search = () => {
-    const [term, setTerm] = useState('');
+    
+    const [term, setTerm] = useState('Programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
+
+    useEffect(()=>{
+        const timerId = setTimeout(()=>{
+            setDebouncedTerm(term);
+        },1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+    }, [term]);
+
 
     useEffect(()=>{
         const search = (async () => {
@@ -13,20 +25,22 @@ const Search = () => {
                     list:'search',
                     format:'json',
                     origin:'*',
-                    srsearch:term
+                    srsearch:debouncedTerm
                 }
             });
 
             setResults(data.query.search);
         });
-        if(term){
-            search();
-        }
-    }, [term]);
+        search();
+    }, [debouncedTerm])
 
     const renderedResults = results.map((result) =>{
+        console.log(result);
         return (
             <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+                </div>
                 <div className="content">
                     <div className="header">{result.title}</div>
                     {result.snippet.replaceAll('<span class="searchmatch">','').replaceAll('</span>','')}
