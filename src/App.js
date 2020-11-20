7899332c7c00c6f2ad30363a6be52110d3676a56
Yongkang Zhao <cookieyong@gmail.com>
2020-11-20 10:25:07 -0800

    started a new widget feature dropdown

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Accordion from './components/Accordion';
 import Search from './components/Search';
-
+import Dropdown from './components/Dropdown';
 const items = [
     {
         title: 'what is react',
@@ -21,7 +21,8 @@ export default () => {
     return (
         <div>
             {/* <Accordion items={items}/> */}
-            <Search />
+            {/* <Search /> */}
+            <Dropdown />
         </div>
     );
 }
