94b671d38e3d4803cbbd20abd571adfb72c02c9f
Yongkang Zhao <cookieyong@gmail.com>
2020-11-22 14:45:49 -0800

    added a debounced mechanisim to prevent the api being invoked too many times

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 // import Accordion from './components/Accordion';
 // import Search from './components/Search';
-import Dropdown from './components/Dropdown';
+// import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
 
 // const items = [
