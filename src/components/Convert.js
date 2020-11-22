50e209858cd881e5879ac1f6cbadf4b998a3eb19
Yongkang Zhao <cookieyong@gmail.com>
2020-11-22 13:58:31 -0800

    implemented a basic post to googleapis.com

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,9 +1,16 @@
 import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const Convert = ({language, text}) => {
 
     useEffect(()=>{
-        console.log('new language or text');
+        axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+            params: {
+                q: text,
+                target: language.value,
+                key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+            }
+        });
     }, [language, text])
 
 
